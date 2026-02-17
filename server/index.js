32cb7de6a5f578c27f467ee0dfb00d1a6004b0a0
peregrinthought <peregrine.thought@gmail.com>
2026-02-17 23:16:57 +0530

    Fix missing fs import

--- server/index.js
+++ server/index.js
@@ -2,6 +2,7 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const cors = require('cors');
 const path = require('path');
+const fs = require('fs');
 const db = require('./database');
 
 const app = express();
